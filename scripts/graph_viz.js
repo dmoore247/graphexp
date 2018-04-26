a825e7bb9a3b1766ff061cd88bd1b185169da5d6
Douglas M <Douglas.Moore@ThinkBigAnalytics.com>
2018-04-25 23:15:15 -0400

    add analytic queries, lineage, decendents, ancestors

--- scripts/graph_viz.js
+++ scripts/graph_viz.js
@@ -19,92 +19,71 @@ limitations under the License.
 var graph_viz = (function () {
 	"use strict";
 
-	var _label = ''; // DOM location for visual.
+	var _label = ''; 	// DOM location for visual.
 	var _netvisual = {}; // for visjs.org
-	var _svg = {};
-	var _svg_width = 0;
-	var _svg_height = 0;
-	var _nodes = {};
-	var _edges = {};
-	var _Nodes = [];
-	var _edges = [];
-
+	var _menu = [];		// contextMenu.js
 
 	function init(label) {
-		console.log("Init " + label);
 		_label = label;
 	}
 
 	///////////////////////////////////////
 	// Remove force layout and data
 	function clear() {
-		if (_netvisual) {
+		if (_netvisual && _netvisual != {} && _netvisual.destroy) {
 			_netvisual.destroy();
 			_netvisual = {};
+			$('.menuContainer').contextMenu('destroy');
+			_menu = [];
 		}
-		layers.clear_old();
 	}
 
 	function status(message) {
 		$('#messageArea').html("<h3>" + message + "</h3>");
 	}
 
-	var menu = [{
-		name: 'lineage',
-		//			img: 'images/create.png',
-		title: 'Entire Lineage',
-		fun: function (data, e) {
-			console.log('Entire Lineage');
-			console.log(data);
-			console.log(e);
-			var nodes = _netvisual.getSelectedNodes();
-			console.log(nodes);
-		}
-	}, {
-		name: 'neighbors',
-		title: 'Display Neighbors',
-		fun: function () {
-			showNeighbors(_netvisual.getSelectedNodes());
-		}
-	}, {
-		name: 'cluster',
-		title: 'Cluster descendents',
-		fun: function () {
-			var nodes = _netvisual.getSelectedNodes();
-			clusterByNodeId(nodes);
-		}
-	}, {
-		name: 'uncluster',
-		title: 'Uncluster cluster',
-		fun: function (data, e) {
-			var nodes = _netvisual.getSelectedNodes();
-			unClusterByNodeId(nodes);
-		}
-	}, {
-		name: 'hide',
-		title: 'Hide node from view',
-		fun: function () {
-			console.log('Hide');
-		}
-	}
-	];
-
 	// install context menu
 	function init_context_menus() {
-		console.log("initializing menu");
-
-		// install context menu to override browser menu
-		$('.menuContainer').contextMenu(menu, { triggerOn: 'contextmenu' });
+		_menu.push( { name: 'Show Neighbors', fun: function() {showNeighbors(_netvisual.getSelectedNodes())}});
+		_menu.push( { name: 'Show Full Lineage', fun: function() {showLineage(_netvisual.getSelectedNodes())}});
+		_menu.push( { name: 'Show Decendents', fun: function() {showDecendents(_netvisual.getSelectedNodes())}});
+		_menu.push( { name: 'Show Ancestors', fun: function() {showAncestors(_netvisual.getSelectedNodes())}});
+		_menu.push( { name: 'Cluster Node', fun: function() {clusterByNodeId(_netvisual.getSelectedNodes())}});
+		_menu.push( { name: 'UnCluster Node', fun: function() {unClusterByNodeId(_netvisual.getSelectedNodes())}});
+		$('.menuContainer').contextMenu(_menu, { triggerOn: 'contextmenu' });
 	}
 
 	function showNeighbors(nodes) {
 		console.log("Show Neighbors");
 		if (nodes.length > 0) {
 			_netvisual.storePositions();
-			graphioGremlin.click_query({ id: nodes[0] });
+			graphioGremlin.neighbors_query({ id: nodes[0] });
 		}
 	}
 
+	function showLineage(nodes) {
+		console.log("Show Lineage");
+		if (nodes.length > 0) {
+			_netvisual.storePositions();
+			graphioGremlin.lineage_query({ id: nodes[0] });
+		}
+	}
+
+	function showDecendents(nodes) {
+		console.log("Show Decendents");
+		if (nodes.length > 0) {
+			_netvisual.storePositions();
+			graphioGremlin.decendents_query({ id: nodes[0] });
+		}
+	}
+
+	function showAncestors(nodes) {
+		console.log("Show Ancestors");
+		if (nodes.length > 0) {
+			_netvisual.storePositions();
+			graphioGremlin.ancestors_query({ id: nodes[0] });
+		}
+	}
 	// cluster entire network
 	function cluster() {
 		if (_netvisual) {
@@ -112,11 +91,10 @@ var graph_viz = (function () {
 			_netvisual.clusterOutliers();
 		}
 	}
-	
+
 	function clusterByNodeId(nodes) {
 		console.log("Cluster");
 		if (nodes.length > 0) {
-//			_netvisual.storePositions();
 			_netvisual.clusterByConnection(nodes[0]);
 		}
 	}
@@ -124,154 +102,18 @@ var graph_viz = (function () {
 	function unClusterByNodeId(nodes) {
 		console.log("unCluster");
 		if (nodes.length > 0) {
-//			_netvisual.storePositions();
 			_netvisual.openCluster(nodes[0]);
 		}
 	}
 
-	//////////////////////////////////////////////////////////////
-	var layers = (function () {
-		// Submodule that handles layers of visualization
-
-		var nb_layers = default_nb_of_layers;
-		var old_Nodes = [];
-		var old_edges = [];
-
-		function set_nb_layers(nb) {
-			nb_layers = nb;
-		}
-
-		function depth() {
-			return nb_layers;
-		}
-
-		function push_layers() {
-			// old edges and nodes become older
-			// and are moved to the next deeper layer
-			for (var k = nb_layers; k > 0; k--) {
-				var kp = k - 1;
-				_svg.selectAll(".old_edge" + kp).classed("old_edge" + k, true);
-				_svg.selectAll(".old_node" + kp).classed("old_node" + k, true);
-				_svg.selectAll(".old_edgepath" + kp).classed("old_edgepath" + k, true);
-				_svg.selectAll(".old_edgelabel" + kp).classed("old_edgelabel" + k, true);
-			};
-		}
-
-		function clear_old() {
-			old_Nodes = [];
-			old_edges = [];
-		}
-
-		function update_data(d) {
-			// Save the data
-			var previous_nodes = _svg.selectAll("g").filter(".active_node");
-			var previous_nodes_data = previous_nodes.data();
-			old_Nodes = updateAdd(old_Nodes, previous_nodes_data);
-			var previous_edges = _svg.selectAll(".active_edge");
-			var previous_edges_data = previous_edges.data();
-			old_edges = updateAdd(old_edges, previous_edges_data);
-
-			// handle the pinned nodes
-			var pinned_Nodes = _svg.selectAll("g").filter(".pinned");
-			var pinned_nodes_data = pinned_Nodes.data();
-			// get the node data and merge it with the pinned nodes
-			_Nodes = d.nodes;
-			_Nodes = updateAdd(_Nodes, pinned_nodes_data);
-			// add coordinates to the new active nodes that already existed in the previous step
-			_Nodes = transfer_coordinates(_Nodes, old_Nodes);
-			// retrieve the edges between nodes and pinned nodes
-			_edges = d.edges.concat(previous_edges_data); // first gather the edges
-			_edges = find_active_edges(_edges, _Nodes); // then find the ones that are between active nodes
-
-		}
-
-		function updateAdd(array1, array2) {
-			// Update lines of array1 with the ones of array2 when the elements' id match
-			// and add elements of array2 to array1 when they do not exist in array1
-			var arraytmp = array2.slice(0);
-			var removeValFromIndex = [];
-			array1.forEach(function (d, index, thearray) {
-				for (var i = 0; i < arraytmp.length; i++) {
-					if (d.id == arraytmp[i].id) {
-						thearray[index] = arraytmp[i];
-						removeValFromIndex.push(i);
-					}
-				}
-			});
-			// remove the already updated values (in reverse order, not to mess up the indices)
-			removeValFromIndex.sort();
-			for (var i = removeValFromIndex.length - 1; i >= 0; i--)
-				arraytmp.splice(removeValFromIndex[i], 1);
-			return array1.concat(arraytmp);
-		}
-
-		function find_active_edges(list_of_edges, active_nodes) {
-			// find the edges in the list_of_edges that are between the active nodes and discard the others
-			var active_edges = [];
-			list_of_edges.forEach(function (row) {
-				for (var i = 0; i < active_nodes.length; i++) {
-					for (var j = 0; j < active_nodes.length; j++) {
-						if (active_nodes[i].id == row.source.id && active_nodes[j].id == row.target.id) {
-							var L_data = { source: row.source.id, target: row.target.id, type: row.type, value: row.value, id: row.id };
-							var L_data = row;
-							L_data['source'] = row.source.id;
-							L_data['target'] = row.target.id;
-							active_edges = active_edges.concat(L_data);
-						}
-						else if (active_nodes[i].id == row.source && active_nodes[j].id == row.target) {
-							var L_data = row;
-							active_edges = active_edges.concat(L_data);
-						}
-					}
-				}
-			});
-			// the active edges are in active_edges but there can be some duplicates
-			// remove duplicates edges
-			var dic = {};
-			for (var i = 0; i < active_edges.length; i++)
-				dic[active_edges[i].id] = active_edges[i]; // this will remove the duplicate edges (with same id)
-			var list_of_active_edges = [];
-			for (var key in dic)
-				list_of_active_edges.push(dic[key]);
-			return list_of_active_edges;
-		}
-
-
-		function transfer_coordinates(Nodes, old_Nodes) {
-			// Transfer coordinates from old_nodes to the new nodes with the same id
-			for (var i = 0; i < old_Nodes.length; i++) {
-				var exists = 0;
-				for (var j = 0; j < Nodes.length; j++) {
-					if (Nodes[j].id == old_Nodes[i].id) {
-						Nodes[j].x = old_Nodes[i].x;
-						Nodes[j].y = old_Nodes[i].y;
-						Nodes[j].fx = old_Nodes[i].x;
-						Nodes[j].fy = old_Nodes[i].y;
-						Nodes[j].vx = old_Nodes[i].vx;
-						Nodes[j].vy = old_Nodes[i].vy;
-					}
-				}
-			}
-			return Nodes;
-		}
-
-		return {
-			set_nb_layers: set_nb_layers,
-			depth: depth,
-			push_layers: push_layers,
-			clear_old: clear_old,
-			update_data: update_data
-		}
-	})();
-
-	function refresh_data(d, center_f, with_active_node) {
+	function refresh_data(d, query_type) {
 
 		status("redrawing");
 		var label_property = "name";
 		var label_property2 = "type";
 		const text_property = "text";
 
-		console.log(d);
+		// console.log(d);
 		graph_data.update({
 			nodes: d.nodes.map(v => ({
 				id: v.id,      	// vertex id
@@ -286,15 +128,13 @@ var graph_viz = (function () {
 		// map graph data set edges to internal edges.
 		var dataIO = graph_data.data();
 		var containerIO = document.getElementById(_label);
-		console.log("Draw:")
-		console.log(_label, containerIO, dataIO, graph_options);
 		if (jQuery.isEmptyObject(_netvisual)) {
 			status("New drawing");
 			init_context_menus();
 			_netvisual = new vis.Network(containerIO, dataIO, graph_options);
 			_netvisual.on("selectNode", function (params) {
 				if (params.nodes.length > 0 && params.nodes[0] && !_netvisual.isCluster(params.nodes[0])) {
-					infobox.display_info(graph_data.node(params.nodes[0]));	
+					infobox.display_info(graph_data.node(params.nodes[0]));
 				}
 			});
 			/*
@@ -307,7 +147,7 @@ var graph_viz = (function () {
 				showNeighbors(params.nodes);
 			});
 			_netvisual.on("startStabilizing", function (params) {
-				console.log("startStabilizing");
+				//console.log("startStabilizing");
 			});
 			_netvisual.stabilize(50);
 			_netvisual.startSimulation();
@@ -318,112 +158,11 @@ var graph_viz = (function () {
 		}
 	}
 
-	
-
-	function get_node_edges(node_id) {
-		// Return the in and out edges of node with id 'node_id'
-		var connected_edges = d3.selectAll(".edge").filter(
-			function (item) {
-				if (item.source == node_id || item.source.id == node_id) {
-					return item;
-				}
-				else if (item.target == node_id || item.target.id == node_id) {
-					return item;
-				}
-			});
-		return connected_edges;
-	}
-
-
-	var graph_events = (function () {
-		//////////////////////////////////
-		// Handling mouse events
-
-		function dragstarted(d) {
-			d.fx = d.x;
-			d.fy = d.y;
-		}
-
-		function dragged(d) {
-			var connected_edges = get_node_edges(d.id);
-			var f_connected_edges = connected_edges.filter("*:not(.active_edge)")
-			if (f_connected_edges._groups[0].length == 0) {
-				d.fx = d3.event.x;
-				d.fy = d3.event.y;
-			}
-			else {
-				f_connected_edges
-					.style("stroke-width", function () { return parseInt(d3.select(this).attr("stroke-width")) + 2; })
-					.style("stroke-opacity", 1)
-					.classed("blocking", true)
-			}
-		}
-
-		function dragended(d) {
-			d3.selectAll(".blocking")
-				.style("stroke-width", function () { return d3.select(this).attr("stroke-width"); })
-				.style("stroke-opacity", function () { return d3.select(this).attr("stroke-opacity"); })
-				.classed("blocking", false)
-			// d.fx = null;
-			// d.fy = null;
-		}
-
-		function clicked(d) {
-			var input = document.getElementById("freeze-in");
-			var isChecked = input.checked;
-			if (isChecked) infobox.display_info(d);
-			else {
-				// remove the oldest edges and nodes
-				var stop_layer = layers.depth() - 1;
-				_svg.selectAll(".old_node" + stop_layer).remove();
-				_svg.selectAll(".old_edge" + stop_layer).remove();
-				_svg.selectAll(".old_edgepath" + stop_layer).remove();
-				_svg.selectAll(".old_edgelabel" + stop_layer).remove();
-				infobox.display_info(d);
-				graphioGremlin.click_query(d);
-				console.log('event!!')
-			}
-		}
-
-
-		function pin_it(d) {
-			d3.event.stopPropagation();
-			var node_pin = d3.select(this);
-			var pinned_node = d3.select(this.parentNode);
-			//console.log('Pinned!')
-			//console.log(pinned_node.classed("node"));
-			if (pinned_node.classed("active_node")) {
-				if (!pinned_node.classed("pinned")) {
-					pinned_node.classed("pinned", true);
-					console.log('Pinned!');
-					node_pin.attr("fill", "#000");
-					pinned_node.moveToFront();
-				}
-				else {
-					pinned_node.classed("pinned", false);
-					console.log('Unpinned!');
-					node_pin.attr("fill", graphShapes.node_color);
-				}
-			}
-		}
-
-		return {
-			dragstarted: dragstarted,
-			dragged: dragged,
-			dragended: dragended,
-			clicked: clicked,
-			pin_it: pin_it
-		}
-
-	})();
-
 	return {
 		init: init,
 		clear: clear,
 		status: status,
 		refresh_data: refresh_data,
-		layers: layers,
-		graph_events: graph_events,
 		cluster: cluster
 	};
 
